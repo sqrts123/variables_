f0400c04bc31cbe27fc84363ba59e67b56e9bd06
Rhys Wilcoxen <wilcorhy001@warren.k12.in.us>
2024-08-15 09:46:23 -0400

    eeeeeeeeeeeeeeeeeeeeeeeee

---
+++ main.py
@@ -0,0 +1,7 @@
+num_apples = 20
+num_oranges = 15
+print("number of apples:", num_apples)
+print("number of oranges:", num_oranges)
+num_oranges = 0
+print("number of apples:", num_apples)
+print("number of oranges:", num_oranges)
